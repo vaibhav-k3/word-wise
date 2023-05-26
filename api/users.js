08e7eda756106d6a0cdc05d0b20c078d1fcacae0
Vaibhav Kulkarni <vakulk@iu.edu>
2023-05-26 01:37:08 -0400

    let push

--- api/users.js
+++ api/users.js
@@ -3,7 +3,28 @@ const router = express.Router()
 
 DUMMY_USER = {
     'userName':'test',
-    'userPassword':'Pass'
+    'userPassword':'Pass',
+    'words' : ['capricious', 'irate','cajole'],
+    'test_scores':[
+        {
+            'test_type':'Revision',
+            'date': date('2023-01-01'),
+            'score': {
+                'easy':3,
+                'medium':4,
+                'hard':10
+            }
+        },
+        {
+            'test_type':'Revision',
+            'date': date('2023-01-01'),
+            'score': {
+                'easy':3,
+                'medium':4,
+                'hard':10
+            }
+        }
+    ]
 }
 router.get('/getUser',(req,res)=>{
     res.json(DUMMY_USER)
--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express")
 const app = express()
 const userRouter = require('./api/users')
 
+const logger = (req, res, next) => {
+    console.log("request received")
+    next()
+}
+app.use(logger)
 app.use('/api/users',userRouter)
 
 app.listen(5000)
